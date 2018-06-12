184bb0e55a62d29d7883d4d364fd453239383673
nmarsollier <nmarsollier@gmail.com>
2018-06-12 00:08:25 -0300

    Dejar de pelear con el proyecto go en este repositorio y crear un repositorio especifico

--- auth_go/src/user/errors.go
+++
@@ -1,8 +0,0 @@
-package user
-
-import "auth/tools/errors"
-
-var InvalidUserIdError = errors.NewValidationErrorError("id", "Invalid")
-var InvalidLoginError = errors.NewValidationErrorError("login", "Invalid")
-var LoginAlreadyExistError = errors.NewValidationErrorError("login", "Ya existe")
-var InvalidPasswordError = errors.NewValidationErrorError("password", "Invalid")
