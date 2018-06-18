3d44b919f0516056efacaa679e0333f344ed2759
Ariel Parra <arielp@eventbrite.com>
2018-06-17 23:43:28 -0300

    Agregando unit tests para llamadas basicas

--- catalog/application/app.py
+++ catalog/application/app.py
@@ -9,16 +9,25 @@ from flask_cors import CORS
 class MainApp:
 
   def __init__(self):
-    self.flask_app = flask.Flask(__name__)
+    self.flask_app = flask.Flask(__name__, static_folder = '../public')
     CORS(self.flask_app, supports_credentials=True, automatic_options=True)
 
+    self._init_routes()
     self._init_rabbit()
     self._init_articles()
     self._init_api_doc()
-    self._init_routes()
 
-  def get_flask_app(self):
-    return self.flask_app
+  def _init_routes(self):
+    # Servidor de archivos estáticos de apidoc
+    # Por el momento se genera con ../auth/node_modules/.bin/apidoc -i ./ -o public
+    @self.flask_app.route('/<path:path>')
+    def api_index(path):
+        return flask.send_from_directory('../public', path)
+
+
+    @self.flask_app.route('/')
+    def index():
+        return flask.send_from_directory('../public', "index.html")
 
   def _init_rabbit(self):
     rabbitService.init()
@@ -29,17 +38,8 @@ class MainApp:
   def _init_api_doc(self):
     os.system("apidoc -i ./ -o ./public")
 
-  def _init_routes(self):
-    # Servidor de archivos estáticos de apidoc
-    # Por el momento se genera con ../auth/node_modules/.bin/apidoc -i ./ -o public
-    @self.flask_app.route('/<path:path>')
-    def api_index(path):
-        return flask.send_from_directory('public', path)
-
-
-    @self.flask_app.route('/')
-    def index():
-        return flask.send_from_directory('public', "index.html")
+  def get_flask_app(self):
+    return self.flask_app
 
   def start(self):
     self.flask_app.run(port=config.get_server_port())
--- catalog/tests/test_flask.py
+++ catalog/tests/test_flask.py
@@ -15,12 +15,9 @@ def app(mocker):
 
 def test_hello_world(app):
     res = app.get("/")
-    # print(dir(res), res.status_code)
     assert res.status_code == 200
-    assert b"Hello World" in res.data
 
 
 def test_some_id(app):
-    res = app.get("/foo/12345")
+    res = app.get("/#api-Articulos")
     assert res.status_code == 200
-    assert b"12345" in res.data
